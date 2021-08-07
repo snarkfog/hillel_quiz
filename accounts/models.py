e5e0b8b1cc8e7f2516d18def72ed38d0b0a67a44
Maksim Vdovenko <47177162+snarkfog@users.noreply.github.com>
2021-08-07 15:40:17 +0300

    Lesson 22 materials complete.

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True)
+    avatar = models.ImageField(upload_to='profile/', default='default.jpg')
+    birthday = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+
+    def str(self):
+        return self.username
+
+    class Meta(AbstractUser.Meta):
+        pass
